a2d52c285bb557a7dc37479283d28a2a4241f4db
monsieurpunto <puntoriza2@gmail.com>
2025-11-16 23:22:04 +0700

    tambah fitur lihat KTP

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\LihatKartuTandaPendudukController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/lihat-ktp/{kartuTandaPenduduk}', [LihatKartuTandaPendudukController::class, 'lihatKtpHtml'])->name('ktp.view');
